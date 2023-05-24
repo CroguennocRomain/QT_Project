e357e8ef9a19757a1e0cb451818a95f045242db7
mpaitier <mathias.paitier@isen-ouest.yncrea.fr>
2023-05-24 10:50:17 +0200

    Création du CMake

---
+++ cmake-build-debug/CMakeFiles/QT_Project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QT_Project.dir/main.cpp.o"
+  "QT_Project"
+  "QT_Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QT_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
