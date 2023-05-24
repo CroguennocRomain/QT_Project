87dcf46271451b0fad77712052353f69a8b87b7c
mpaitier <mathias.paitier@isen-ouest.yncrea.fr>
2023-05-24 20:26:21 +0200

    Ajout des dossiers de l'archive déposée par le prof

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,9 @@
 #include <QApplication>
-#include <QPushButton>
-#include <QTime>
+#include "MainWindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication application(argc, argv);
-
-    QPushButton button("Hello world!");
-    button.show();
-
+    MainWindow mainWindow;
+    mainWindow.show();
     return application.exec();
 }
