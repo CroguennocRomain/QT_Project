e521878e44184a360ca6cb120d71397632a9b9a1
mpaitier <mathias.paitier@isen-ouest.yncrea.fr>
2023-06-10 16:16:40 +0200

    Commentaires et rename Le jeu est complet, il manque seulement la rejouablité, scoreboard et menu principale Ajouts possibles au gameplay -> PV aliens fonctionnels -> Score aliens fonctionnels

---
+++ cmake-build-debug/CMakeFiles/QT_Project.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QT_Project_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/QT_Project_autogen.dir/ParseCache.txt"
+  "QT_Project_autogen"
+  "CMakeFiles/QT_Project.dir/QT_Project_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/QT_Project.dir/src2/Game.cpp.o"
+  "CMakeFiles/QT_Project.dir/src2/component.cpp.o"
+  "CMakeFiles/QT_Project.dir/src2/main.cpp.o"
+  "QT_Project"
+  "QT_Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QT_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
