c680a2a61d59a5b3b77d761335403c2436363c8b
mpaitier <mathias.paitier@isen-ouest.yncrea.fr>
2023-06-10 17:53:36 +0200

    Scoreboard déplacé dans Game

--- src2/main.cpp
+++ src2/main.cpp
@@ -2,12 +2,12 @@
 #include "Scoreboard.h"
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    Scoreboard* pGame = new Scoreboard();
+    Game* pGame = new Game();
     //Game* pGame = new Game();
     pGame->show();
     // Avoir la fenetre d'une taille précise
     pGame->setFixedSize(400, 800);
-    //pGame->run();
+    pGame->run();
 
     return a.exec();
 }
